02968942a4d92b9a4f1a611f52d659a073cf4097
@little_prince <13510222833@139.com>
2023-07-21 23:19:19 +0800

    add nuget

--- EmailClient/EmailSystemDatabase.cs
+++ EmailClient/EmailSystemDatabase.cs
@@ -2,7 +2,6 @@ using System;
 using System.Data.SqlClient;
 using System.Net;
 using System.Net.Mail;
-using OpenPop.Pop3;
 
 public class EmailSystem
 {
@@ -51,45 +50,7 @@ public class EmailSystem
         }
     }
 
-    public void SendEmail(string recipient, string subject, string body)
-    {
-        // 发送邮件
-        using (var client = new SmtpClient(SmtpServer, SmtpPort))
-        {
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential(Username, Password);
-
-            var message = new MailMessage(Username, recipient, subject, body);
-            client.Send(message);
-
-            // 保存到发件箱
-            SaveEmailToSentItems(Username, recipient, subject, body);
-        }
-    }
-
-    public void ReceiveEmails()
-    {
-        using (var client = new Pop3Client())
-        {
-            client.Connect(Pop3Server, Pop3Port, true);
-            client.Authenticate(Username, Password);
-
-            int messageCount = client.GetMessageCount();
-            for (int i = messageCount; i > 0; i--)
-            {
-                var message = client.GetMessage(i);
-                // 处理邮件，可以获取主题、发件人、正文等信息
-                Console.WriteLine($"Subject: {message.Headers.Subject}");
-                Console.WriteLine($"From: {message.Headers.From}");
-                Console.WriteLine($"Body: {message.MessagePart.GetBodyAsText()}");
-
-                // 保存到收件箱
-                SaveEmailToInbox(message.Headers.From.Address, Username, message.Headers.Subject, message.MessagePart.GetBodyAsText());
-            }
-        }
-    }
-
-    private void SaveEmailToInbox(string sender, string recipient, string subject, string body)
+    public void SaveEmailToInbox(string sender, string recipient, string subject, string body)
     {
         string insertEmailQuery = "INSERT INTO Inbox (Sender, Recipient, Subject, Body, DateTime) VALUES (@Sender, @Recipient, @Subject, @Body, @DateTime)";
 
@@ -110,7 +71,7 @@ public class EmailSystem
         }
     }
 
-    private void SaveEmailToOutbox(string sender, string recipient, string subject, string body)
+    public void SaveEmailToOutbox(string sender, string recipient, string subject, string body)
     {
         string insertEmailQuery = "INSERT INTO Outbox (Sender, Recipient, Subject, Body, DateTime) VALUES (@Sender, @Recipient, @Subject, @Body, @DateTime)";
 
