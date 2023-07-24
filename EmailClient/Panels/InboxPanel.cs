4d40b82c3742c2876160fd02f058cb6d0041dd2f
@little_prince <13510222833@139.com>
2023-07-24 12:09:11 +0800

    update mime construct

--- EmailClient/Panels/InboxPanel.cs
+++ EmailClient/Panels/InboxPanel.cs
@@ -17,48 +17,71 @@ namespace EmailClient.Panels
         string password;
         Pop3Client pop3Client;
         List<string> emails;
+        List<string> messageIds;
+        int numOfEmails;
 
+       
         public InboxPanel(string emailAddress, string password)
         {
             InitializeComponent();
             Setup();
-            this.emailAddress= emailAddress;
-            this.password= password;
+            this.emailAddress = emailAddress;
+            this.password = password;
+            numOfEmails = 0;
+            messageIds = new List<string>();
             
+            pop3Client = new Pop3Client("pop.qq.com", 995);
+            pop3Client.Connect(emailAddress, password);
         }
+
+        
 
         public void FetchEmail()
         {
-            
             try
             {
-                pop3Client = new Pop3Client("pop.qq.com", 995);
-                pop3Client.Connect(emailAddress, password);
+                if (numOfEmails == pop3Client.GetEmailNum())
+                    return;
+
+                InboxListView.Items.Clear();
                 emails = pop3Client.GetEmails();
 
                 foreach (string emailStr in emails)
                 {
                     Email email = Email.Parse(emailStr);
-                    ListViewItem item = new ListViewItem(email.sender);
-                    item.SubItems.Add(email.subject);
-                    InboxListView.Items.Add(item);
+                    if (messageIds.Contains(email.id))
+                        continue;
+                    messageIds.Add(email.id);
+                    Action action = () => AddEmailToListView(email);
+                    Invoke(action);
+                    numOfEmails++;
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+
+        }
+
+        public void AddEmailToListView(Email email)
+        {
+            ListViewItem item = new ListViewItem(email.sender);
+            item.SubItems.Add(email.subject);
+            item.SubItems.Add(email.date);
+            InboxListView.Items.Add(item);
         }
 
         private void Setup()
         {
             InboxListView.FullRowSelect = true;
-        
+            
             //InboxListView.GridLines = true;
             InboxListView.View = View.Details;
-            InboxListView.Columns.Add("sender", -100, HorizontalAlignment.Left);
-            InboxListView.Columns.Add("subject", -120, HorizontalAlignment.Left);
-            
+            //InboxListView.Columns.Add("发送者", -100, HorizontalAlignment.Left);
+            //InboxListView.Columns.Add("主题", -120, HorizontalAlignment.Left);
+            //InboxListView.Columns.Add("时间", -100, HorizontalAlignment.Left);
+            //InboxListView.Show();
         }
 
         
@@ -73,6 +96,12 @@ namespace EmailClient.Panels
         private void InboxListView_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void GetButton_Click(object sender, EventArgs e)
+        {
+            pop3Client.Connect(emailAddress, password);
+            FetchEmail();
         }
     }
 }
